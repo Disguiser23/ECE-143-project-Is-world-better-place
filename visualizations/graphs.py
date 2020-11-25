64587e6d62aa58afa17bd2aac81e120c1abf7ed5
ptsun0909 <terry22887353@gmail.com>
2020-11-25 14:31:23 -0800

    add three python file and one notebook to run all

--- visualizations/graphs.py
+++ visualizations/graphs.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append('../')
 from utils import colors_pastel
+import pandas as pd
 
 def plot_prediction_line_graph(data, pred_df, xlabel, ylabel, title, filename):
     '''xlabel, ylabel: str axis labels
@@ -17,6 +19,7 @@ def plot_prediction_line_graph(data, pred_df, xlabel, ylabel, title, filename):
     ax.set_ylabel(ylabel)
     plt.title(title)
     plt.savefig(filename, bbox_inches='tight')
+    plt.show()
 
 
 def stacked_bar_graph_prediction(data, filename="stacked_bar_graph.png", title='title', ylabel="Amount"):
@@ -26,3 +29,4 @@ def stacked_bar_graph_prediction(data, filename="stacked_bar_graph.png", title='
     ax =  data.plot(kind='bar', stacked=True, color=colors_pastel, rot=70, title=title)
     ax.set_ylabel(ylabel)
     plt.savefig(filename, bbox_inches='tight')
+    plt.show()
